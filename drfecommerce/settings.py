63a43cf73886bd283a6d2a1069cbce3d603f3dc7
frankassafuah <frankassafuah@gmail.com>
2024-10-06 15:42:59 +0000

    create models

--- drfecommerce/settings.py
+++ drfecommerce/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = "drfecommerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ecommerce_api",
+        "USER": "root",
+        "PASSWORD": "Frank0268",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
