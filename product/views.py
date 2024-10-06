24f4ae4b2aa4f0cd87ef109401390b491290a071
frankassafuah <frankassafuah@gmail.com>
2024-10-06 16:44:29 +0000

    create category viewset and register to router

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ViewSet):
+
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
